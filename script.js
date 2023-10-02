94aa55fa6d6a62c96e1da2b43083febd3c10fcbc
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-10-02 06:49:46 -0300

    adicionando eventos de click

--- script.js
+++ script.js
@@ -2,4 +2,10 @@
 var elementosDuvida = document.querySelectorAll(".duvida");
 
 // forEach executa uma função para cada uma das duvidas
-elementosDuvida.forEach(function (duvida) {});
+//ClassList é uma lista de classes
+//addEventListener adicionar um elemnto para o javascript monitorar
+elementosDuvida.forEach(function (duvida) {
+  duvida.addEventListener("click", function () {
+    duvida.classList.add("fundo-verde");
+  });
+});
