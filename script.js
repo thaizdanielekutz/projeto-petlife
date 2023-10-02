5e51af1809749cd7ca451818a64ba550d11d1b93
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-10-02 06:44:53 -0300

    função forEach

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 //eventos: coisas que acontecem na página
 var elementosDuvida = document.querySelectorAll(".duvida");
+
+// forEach executa uma função para cada uma das duvidas
+elementosDuvida.forEach(function (duvida) {});
