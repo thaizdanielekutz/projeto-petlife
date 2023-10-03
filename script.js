1cbb9011b51f23a2bb93c210ea08188288ae890c
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-10-03 06:56:33 -0300

    ajustes finais de código

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ var elementosDuvida = document.querySelectorAll(".duvida");
 // forEach executa uma função para cada uma das duvidas
 //ClassList é uma lista de classes
 //addEventListener adicionar um elemnto para o javascript monitorar
+//toggle
 elementosDuvida.forEach(function (duvida) {
   duvida.addEventListener("click", function () {
-    duvida.classList.add("fundo-verde");
+    duvida.classList.toggle("ativa");
   });
 });
